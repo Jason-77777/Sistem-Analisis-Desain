357246d38215c66bae42b6df4a0a6fe2c2f7c579
Jason-77777 <126946180+Jason-77777@users.noreply.github.com>
2023-12-12 12:27:09 +0700

    hal 1 hmpr siap

--- ALL INSIDE/mainpage.js
+++ ALL INSIDE/mainpage.js
@@ -9,16 +9,61 @@ function showSlides() {
         slides[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex > slides.length) { slideIndex = 1 }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); 
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000);
 }
 
 function showDetails() {
     // Logika untuk menampilkan detail film
     alert("Movie details will be shown here.");
 }
+
+let currentSet = 0;
+    const totalSets = 2; // Total set gambar
+    const images = [
+        [
+            "Ghibli Inspired Wallapaper.jpg",
+            "coffee shop with warm atmosphere [ wallpaper 4k ].jpg",
+            "download.jpg"
+        ],
+        [
+            // Tambahkan 3 gambar lagi untuk set kedua
+            "mesin raksasa.jpg",
+            "orang melamun.jpg",
+            "rumah hobbit.jpg"
+        ]
+    ];
+
+    function showDetails() {
+        // Logika untuk menampilkan detail film
+        alert("Movie details will be shown here.");
+    }
+
+    function changeSlide(direction) {
+        const movieCarousel = document.querySelector('.movie-carousel');
+
+        currentSet += direction;
+
+        if (currentSet >= totalSets) {
+            currentSet = 0;
+        } else if (currentSet < 0) {
+            currentSet = totalSets - 1;
+        }
+
+        updateImage(images[currentSet]);
+        const translateValue = -100 * currentSet;
+        movieCarousel.style.transform = `translateX(${translateValue}%)`;
+    }
+
+    function updateImage(imageSet) {
+        const imageElements = document.querySelectorAll('.movie-entry img');
+
+        for (let i = 0; i < 3; i++) {
+            imageElements[i].src = imageSet[i];
+        }
+    }
